8eefe5052ce5b81195744d27608fbe8314549da2
Artrix <artrix@126.com>
2017-06-12 13:57:32 +0800

    Fixed some bug.

--- Quantumer/Quantumer.py
+++ Quantumer/Quantumer.py
@@ -113,9 +113,16 @@ def simple_reply(msg):
         if g.step == 2:
             g.content = str(msg['Text'])
             g.is_guide = False
-            g.step = 0
+            g.step = 3
             itchat.send("复制以下内容，发送至本账号即可开始监听:", user_name)
+            itchat.send("（如果需要直接开始，请输入“开始”。）", user_name)
             return g.generate()
+        if g.step == 3:
+            g.step = 0
+            if "开始" in str(msg['Text']):
+                m = msg
+                m['Text'] = g.generate()
+                simple_reply(m)
 
     if msg['Type'] == 'Text':
         text = str(msg['Text'])
